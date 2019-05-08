ac5db285ab0d9d42776bedb0887237e3447eb790
duyyudus <duyyudus@gmail.com>
2019-05-08 19:21:20 +0700

    WIP on integrating tree distance mechanism to DiffEngine

--- test/testcase/test_core_diff_engine.py
+++ test/testcase/test_core_diff_engine.py
@@ -1,24 +1,4 @@
-# Test tree, preorder
-#
-# root
-# |---a
-# |---|---a1
-# |---|---|---a1a
-# |---|---|---|---a1a1
-# |---|---|---|---a1a2
-# |---|---a2
-# |---|---|---a2a
-# |---b
-# |---|---b1
-# |---|---|---b1a
-# |---|---b2
-# |---|---|---b2a
-# |---|---|---b2b
-# |---c
-# |---|---c1
-# |---|---c2
-
-# Visualized in levelorder
+# Test trees
 #
 #                    root
 #         ---------------------------
@@ -30,7 +10,15 @@
 # ----------
 # a1a1  a1a2
 #
-# parent_offset = len(children)/2 - len(parent)/2
+#                    root
+#         ------------------------------
+#         a        b1n     b2n     c_rel
+#    -----------   ---   -------   ------
+#    a1       a2   b1a   b2a b2b   c1  c2
+#    ---      ---        ----
+#    a1a      a2a        b2a1
+# ----------
+# a1a1  a1a3
 
 from _setup_test import *
 Tree = tree.Tree
@@ -44,11 +32,36 @@ class TestCoreDiffEngine(unittest.TestCase):
 
     def test_diff_engine(self):
         log_info()
-        t1 = Tree('t1', 'r1')
-        t2 = Tree('t2', 'r2')
+
+        p1 = [
+            'a/a1/a1a/a1a1',
+            'a/a1/a1a/a1a2',
+            'a/a2/a2a',
+            'b/b1/b1a',
+            'b/b2/b2a',
+            'b/b2/b2b',
+            'c/c1',
+            'c/c2',
+        ]
+        t1 = Tree('t1', 'root')
+        t1.build_tree(p1)
+
+        p2 = [
+            'a/a1/a1a/a1a1',
+            'a/a1/a1a/a1a3',
+            'a/a2/a2a',
+            'b1n/b1a',
+            'b2n/b2a/b2a1',
+            'b2n/b2b',
+            'c_rel/c1',
+            'c_rel/c2',
+        ]
+        t2 = Tree('t2', 'root')
+        t2.build_tree(p2)
 
         diff = diff_engine.DiffEngine(t1, t2)
-        diff.compute_edit_sequence()
+        diff.compute_edit_sequence(show_matrix=1, show_edit=1, verbose=1)
+
         diff.postprocess_edit_sequence()
         diff.interpret_diff()
 
--- tree_util_lite/core/diff_engine.py
+++ tree_util_lite/core/diff_engine.py
@@ -1,9 +1,10 @@
 from tree_util_lite.common.util import *
-from tree_util_lite.tree_distance import zhang_shasha
+from tree_util_lite.tree_distance import zhang_shasha, descendant_alignment
 
 
 class TreeDistAlgo(object):
     ZHANG_SHASHA = 'ZHANG_SHASHA'
+    DESCENDANT_ALIGNMENT = 'DESCENDANT_ALIGNMENT'
 
 
 class DiffData(dict):
@@ -34,7 +35,7 @@ class DiffEngine(object):
 
     """
 
-    def __init__(self, tree1, tree2, tree_distance_algo=TreeDistAlgo.ZHANG_SHASHA):
+    def __init__(self, tree1, tree2, tree_distance_algo=TreeDistAlgo.DESCENDANT_ALIGNMENT):
         """
         Args:
             tree1 (tree.Tree):
@@ -46,6 +47,8 @@ class DiffEngine(object):
         self._tree2 = tree2
         if tree_distance_algo == TreeDistAlgo.ZHANG_SHASHA:
             self._tree_distance = zhang_shasha.ZhangShasha(tree1.root, tree2.root)
+        elif tree_distance_algo == TreeDistAlgo.DESCENDANT_ALIGNMENT:
+            self._tree_distance = descendant_alignment.DescendantAlignment(tree1.root, tree2.root)
         else:
             raise Exception('Please choose a tree distance algorithm')
         self._edit_sequence = None
@@ -56,7 +59,7 @@ class DiffEngine(object):
         """DiffData: """
         return self._diff_data
 
-    def compute_edit_sequence(self):
+    def compute_edit_sequence(self, show_matrix=0, show_edit=0, verbose=0):
         """Raw edit sequence, in form of a list of 2-tuple.
 
         Atomic edit operation:
@@ -70,10 +73,37 @@ class DiffEngine(object):
             (None, node_in_tree2): insert edit operation
 
         """
-        self._edit_sequence = self._tree_distance.compute_edit_sequence()
+        if self._tree_distance._TD[0][0] is None:
+            self._tree_distance.compute_tree_distance(verbose=verbose)
+        self._edit_sequence = self._tree_distance.compute_edit_sequence(show_matrix=show_matrix)
+
+        if show_edit:
+            self.render_edit_sequence()
+
+    def render_edit_sequence(self):
+        size = max([len(p[0].label) if p[0] else 0 for p in self._edit_sequence])
+        for p in self._edit_sequence:
+            label_1 = p[0].label if p[0] else '__'
+            label_2 = p[1].label if p[1] else '__'
+            tail_1 = ' ' * (size - len(label_1))
+            if label_1 == '__':
+                mapping = '--insert--->'
+            elif label_2 == '__':
+                mapping = '--delete--->'
+            elif label_1 == label_2:
+                mapping = '----------->'
+            else:
+                mapping = '--relabel-->'
+            print(
+                '  {}{}'.format(label_1, tail_1),
+                mapping,
+                label_2
+            )
 
     def postprocess_edit_sequence(self):
         """Process raw edit sequence to get a more compact diff data.
+
+        Edit sequence from different tree distance algorithms will need its own way of postprocessing.
 
         Returns:
             DiffData:
